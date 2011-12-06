ea7fc6e6b81cf9c5975e7b33644bf8e556b90c27
Roman Imankulov <roman@netangels.ru>
2011-12-06 16:51:10 +0600

    Generated secret path structure for attachments

--- session_attachments/models.py
+++ session_attachments/models.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
+import hashlib
 from django.db import models
+from django.conf import settings
 
 
 class Attachment(models.Model):
+
+    def file_upload_to(instance, filename):
+        md5 = hashlib.md5()
+        md5.update(instance.session_id)
+        md5.update(instance.bundle)
+        md5.update(instance.session_id)
+        md5.update(settings.SECRET_KEY)
+        digest = md5.hexdigest()
+        filename_digest = hashlib.md5(filename).hexdigest()
+        return 'session_attachments/%s/%s/%s' % (digest, filename_digest, filename)
+
     created = models.DateTimeField(auto_now_add=True)
     session_id = models.CharField(max_length=40)
     bundle = models.CharField(max_length=256)
-    file = models.FileField(upload_to='session_attachments')
+    file = models.FileField(upload_to=file_upload_to)
